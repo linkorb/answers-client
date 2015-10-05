9b5c189c00dfee311a5a82c704618d83a0ce256f
Igor Mukhin <igor.mukhin@gmail.com>
2015-10-05 11:03:48 +0300

    Fixed: Code style by php-cs-fixer

--- src/Client.php
+++ src/Client.php
@@ -4,26 +4,26 @@ namespace Answers\Client;
 
 use GuzzleHttp\Client as GuzzleClient;
 
-class Client {
-
-	private $apiUrl;
+class Client
+{
+    private $apiUrl;
     private $username;
     private $password;
  
-    public function __construct( $apiUrl, $username, $password )
+    public function __construct($apiUrl, $username, $password)
     {
         $this->username = $username;
         $this->password = $password;
         $this->apiUrl = $apiUrl;
     }
 
-    public function get() {
-    	return new GuzzleClient([
-    		'base_url' => [ $this->apiUrl  ],
-		    'defaults' => [
-		        'auth'    => [ $this->username ,  $this->password ],
-		    ]
-		]);
+    public function get()
+    {
+        return new GuzzleClient([
+            'base_url' => [ $this->apiUrl  ],
+            'defaults' => [
+                'auth'    => [ $this->username,  $this->password ],
+            ]
+        ]);
     }
-
 }
